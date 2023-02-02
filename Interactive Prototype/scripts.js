284ba21ec8f802c7f684b0d4f653efe898e5933c
Adam Espinosa <adam.espinosa619@gmail.com>
2023-02-01 21:54:47 -0800

    Attempt 1 complete
    hopefully

--- Interactive Prototype/scripts.js
+++ Interactive Prototype/scripts.js
@@ -6,6 +6,9 @@ document.querySelectorAll('div[data-hyper]')
 document.querySelectorAll('a[data-submenu]')
 .forEach(el => el.addEventListener('mouseover', show_submenu));
 
+document.querySelectorAll('.accordion > h2')
+.forEach(el => el.addEventListener('click', accordion));
+
 function show_submenu(event) {
     let over = event.currentTarget.dataset.submenu;
     document.querySelectorAll(`nav#secondary-nav a:not(.${over})`)
@@ -17,4 +20,19 @@ function show_submenu(event) {
 function createHyperlink(event) {
     let link = event.currentTarget.dataset.hyper;
     document.location.href = link;
+}
+
+function accordion(event) {
+    let section = event.currentTarget.parentNode;
+    let panel = section.querySelector('div.panel');
+    let plus = event.currentTarget.querySelector('span.plus');
+    if (!panel.style.maxHeight) {
+        panel.style.maxHeight = panel.scrollHeight + "px";
+        plus.innerText = "-";
+    } else {
+        panel.style.maxHeight = null;
+        plus.innerText = "+";
+    }
+    
+    
 }
